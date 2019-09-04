e58fcb165913fbe3aaf84a2c2044aeed1d32baf3
irlartboy <ollie_king99@outlook.com>
2019-09-04 10:41:45 +1000

    1st push

---
+++ Assets/Scripts/GameEventListener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField] private GameAction[] actions;
+
+    void Update()
+    {
+        if (Application.isEditor)
+        {
+            foreach (var action in actions)
+            {
+                action.Update();
+            }
+        }
+    }
+    private void OnEnable()
+    {
+        foreach (var action in actions)
+        {
+            action.RegisterListener();
+        }
+    }
+    private void OnDisable()
+    {
+        foreach (var action in actions)
+        {
+            action.UnRegisterListener();
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Sword.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Sword : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onMouseDown;
+
+    private void OnMouseDown()
+    {
+        onMouseDown.Invoke();
+    }
+}
